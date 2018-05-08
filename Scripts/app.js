01a13b79127d8b4cf7385d6f24805f7c4ddd13e2
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2018-05-08 14:11:27 -0400

    added IIFE to app.js

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,1 +1,14 @@
 /* Custom JS goes here */
+
+// IIFE - Immediately Invoked Function Expression
+// anonymous self executing function
+(function(){
+
+    function Start() {
+        console.log("App Started!");
+        console.log("----------------------------");
+        console.log("Hello, World!");
+    }
+
+    window.onload = Start;
+})();
